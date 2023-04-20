c3d79c8c90f78448204b18a2216e8cd576e540d9
Andreas Moritz <greipl@hotmail.com>
2023-04-20 15:49:01 +0200

    change result fetching from ID to code

--- src/app/team/service/team-data.service.ts
+++ src/app/team/service/team-data.service.ts
@@ -1,8 +1,8 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Team } from '../dto/team';
-import { Observable, map } from 'rxjs';
+import { Observable, first, map, mergeMap } from 'rxjs';
 import { API_HEADERS } from '../api/api-config';
+import { Team } from '../dto/team';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +16,11 @@ export class TeamDataService {
   }
   getTeam(id: number): Observable<Team> {
     return this.http.get<Team>(`https://free-nba.p.rapidapi.com/teams/${id}`, { headers: API_HEADERS });
+  }
+
+  getTeamByCode(teamCode: string): Observable<Team> {
+    // the utilized API does not have an endpoint to directly grab a team by its team code
+    // thus, the teams are filtered to return the first match
+    return this.getTeams().pipe(mergeMap(data => data), first(team => team.abbreviation === teamCode));
   }
 }
